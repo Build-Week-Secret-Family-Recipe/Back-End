1d097c64b3dafdffad4a7a452f59a7132f7efb91
sarahmarie1976 <sholle7@gmail.com>
2020-10-22 14:09:24 -0500

    prep for testing

--- knexfile.js
+++ knexfile.js
@@ -22,21 +22,14 @@ module.exports = {
       },
     },
   },
-
   testing: {
-    client: "sqlite3",
-    connection: { filename: "./data/test.db3" },
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "dbmigrations",
-    },
-    seeds: { directory: "./data/seeds" },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
+    migrations: { directory: './database/migrations'},
+    seeds: {  directory: './database/seeds'},
   },
 
   production: {
