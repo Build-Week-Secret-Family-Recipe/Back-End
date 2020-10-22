fb6aa2108a24dbea41949f1abcac9a83b48a9abd
sarahmarie1976 <sholle7@gmail.com>
2020-10-22 15:31:50 -0500

    getting error

--- knexfile.js
+++ knexfile.js
@@ -24,19 +24,13 @@ module.exports = {
   },
 
   testing: {
-    client: "sqlite3",
-    connection: { filename: "./data/test.db3" },
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "dbmigrations",
-    },
-    seeds: { directory: "./data/seeds" },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
+    migrations: { directory: './database/migrations'},
+    seeds: {  directory: './database/seeds'},
   },
   testing: {
     client: 'sqlite3',
