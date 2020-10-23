89d35e799728900bdffb0af7f8f16c5df68105a3
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-10-22 21:53:10 -0400

    instructions get all fixed

--- api/models/instructionsModel.js
+++ api/models/instructionsModel.js
@@ -1,33 +1,28 @@
 const { returning } = require("../../data/db_config.js");
-const db =require("../../data/db_config.js");
+const db = require("../../data/db_config.js");
 
 function findById(id) {
-    return db('instructions').where({id}).first();
+  return db("instructions").where("recipes_id", id).first();
 }
 
 const update = async (changes, id) => {
-    const res = await db('instructions').where({ id }).update(changes);
-    return findById(id);
-}
+  const res = await db("instructions").where({ id }).update(changes);
+  return findById(id);
+};
 
 function remove(id) {
-    return db('instructions')
-    .where({ id })
-    .del();
+  return db("instructions").where({ id }).del();
 }
 
 // insert instructions for EXISTING recipe
 function addInstructions(stepData, id) {
-    
-    const fieldsToInsert = stepData.instructions.map(field =>
-        ({ 
-            "step": field.step_number,
-            "instruction_text": field.instructions,
-            "recipes_id": id
-        }));
+  const fieldsToInsert = stepData.instructions.map((field) => ({
+    step: field.step_number,
+    instruction_text: field.instructions,
+    recipes_id: id,
+  }));
 
-        return db('instructions')
-        .insert(fieldsToInsert)
+  return db("instructions").insert(fieldsToInsert);
 }
 
 // // insert instructions for a NEW recipe (no PK exist yet)
@@ -41,16 +36,16 @@ function addInstructions(stepData, id) {
 //         servings: instructions.servings,
 //         users_id: instructions.users_id
 //     })
-//     .returning('id') 
+//     .returning('id')
 //     .then(function (response) {
 //         if(instructions.instructions) {
 //             const fieldsToInsert = instructions.instructions.map(field =>
-//                 ({ 
+//                 ({
 //                     "step": field.step,
 //                     "instruction_text": field.instructions_text,
 //                     "recipes_id": response[0]
 //                 }));
-        
+
 //                 return db('instructions')
 //                 .insert(fieldsToInsert)
 //         }
@@ -58,9 +53,9 @@ function addInstructions(stepData, id) {
 //   }
 
 module.exports = {
-    findById, 
-    update,
-    remove,
-    addInstructions,
-    // addRecipe, 
-}
+  findById,
+  update,
+  remove,
+  addInstructions,
+  // addRecipe,
+};
