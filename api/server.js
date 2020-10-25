791db1d32b2aee8e9b40316664c7a69bab007040
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-10-24 21:39:18 -0400

    server / endpoint info displayed

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-
 const server = express();
 
 // Routers
@@ -23,9 +22,9 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/auth", authRouter);
-server.use("/api/recipes",   restrictedMiddleware, recipesRouter);
+server.use("/api/recipes", restrictedMiddleware, recipesRouter);
 server.use("/api/ratings", restrictedMiddleware, ratingsRouter);
-
+// server.use("/api/instructions", recipesRouter);
 server.use("/api/instructions", restrictedMiddleware, instructionsRouter);
 server.use("/api/tags", restrictedMiddleware, tagsRouter);
 server.use("/api/ingredients", restrictedMiddleware, ingredientsRouter);
@@ -36,10 +35,12 @@ server.use(
 );
 server.use("/api/recipe_tags", restrictedMiddleware, recipeTagsRouter);
 
-
-
-server.get("/users", (req, res) => {
-  res.json('all users sent');
+server.get("/", (req, res) => {
+  res.send(
+    `<center><h1 style='margin-top: 20px'>Family Recipes API</h1><div><code>Server Running on Port ${
+      process.env.PORT || 3300
+    }</code></div><div style='margin-top: 70px'><a href='https://documenter.getpostman.com/view/12506286/TVYDeezr'>Postman Documentation</a></div></center>`
+  );
 });
 
 module.exports = server;
